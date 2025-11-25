9f47216ce314b88f1ff67e67a379d50893548e2e
AkshatNegi19 <negiakt05@gmail.com>
2025-11-25 19:04:24 +0530

    Add files via upload

---
+++ server/middleware/userAuth.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const userAuth = async(req,res,next)=>{
+    const {token} = req.cookies;
+
+    if (!token) {
+        return res.json({success:false,message:"Not Authorized Login Again"});
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.userId = decoded.id;  
+
+        next();
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+};
+
+export default userAuth;
