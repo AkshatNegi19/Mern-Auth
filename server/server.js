4ceb8d5615a5ad2ddbd110c565ce7bd07b37592d
AkshatNegi19 <negiakt05@gmail.com>
2026-01-28 12:50:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,11 +10,23 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [
+  "https://mern-auth-pi-woad.vercel.app",
+  "http://localhost:5173"
+];
 
 app.use(express.json());
 app.use(cookieParser());    
-app.use(cors({origin:allowedOrigins,credentials:true}));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 // API Endpoints 
 app.get("/",(req,res)=>res.send("API Working"));
