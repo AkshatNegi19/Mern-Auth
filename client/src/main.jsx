069a86e402665269a0b57364c84bd21a37db1e84
AkshatNegi19 <negiakt05@gmail.com>
2026-01-29 14:24:27 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { AppContextProvider } from "./context/AppContext.jsx";
-import axios from "axios";
+import { StrictMode } from 'react'; 
+import { createRoot } from 'react-dom/client'; 
+import App from './App.jsx'; import "./index.css"; 
+import { BrowserRouter } from 'react-router-dom'; 
+import { AppContextProvider } from './context/AppContext.jsx'; 
 
-// ⚡ Configure axios globally
-axios.defaults.withCredentials = true; // send cookies automatically
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL; // set base URL
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render( 
   <StrictMode>
-    <BrowserRouter>
-      <AppContextProvider>
-        <App />
-      </AppContextProvider>
-    </BrowserRouter>
+  <BrowserRouter> 
+  <AppContextProvider> 
+    <App /> 
+  </AppContextProvider> 
+  </BrowserRouter>, 
   </StrictMode>
-);
+)
